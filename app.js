e0b4b0568f0136714b60bc4312638ddf6a02212c
JC Park <whdekf@naver.com>
2024-04-04 13:44:49 +0900

    feat : static webserver

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const route = require('./route');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const server = http.createServer(route.handler);
+app.use('/admin', adminRoutes);
+app.use('/', shopRoutes);
 
-server.listen(3000);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+
+
+
+app.listen(3000);
