c9f67e31815456352c10add2bb2e839f4a8f913f
JC Park <whdekf@naver.com>
2024-04-17 16:34:57 +0900

    chore : remove log

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,6 @@ const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
