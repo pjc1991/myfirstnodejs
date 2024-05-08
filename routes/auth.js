a560f258b3c62ec2a180a5527c205c84771eee14
JC Park <whdekf@naver.com>
2024-05-09 00:30:13 +0900

    feat : add validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check, body } = require('express-validator');
 const router = express.Router();
+const User = require('../models/user');
 
 const authController = require('../controllers/auth');
 
@@ -9,7 +10,29 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup', 
+    check('email').isEmail().withMessage("Please enter valid email")
+    .custom((email, {req})=> {
+        User.findOne({ email: email})
+        .then(user => {
+            if (user) {
+                return Promise.reject('The email exists already.');
+            }
+            return Promise.resolve();
+        }).catch(err =>{
+            console.log(err);
+        })
+    })
+    .withMessage('The email exists already.')
+    ,
+    body('password').isStrongPassword().withMessage("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character"),
+    body('password2').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords have to match!');
+        }
+        return true;
+    }),
+    authController.postSignup);
 
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
